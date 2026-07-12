471ed4b860e47ea503d81ed83320db2960656590
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-12 10:12:10 +0530

    Created Initial Database

--- src-tauri/src/initial_run.rs
+++ src-tauri/src/initial_run.rs
@@ -1,6 +1,37 @@
 // This file will be used to Handle Initial Run of the application and build database
+
+use rusqlite::{params, Connection, OpenFlags, Result};
+use std::fs;
+use std::path::Path;
 use std::path::PathBuf;
+
+const INITIAL_SQL_PROMPT: &str = "CREATE TABLE IF NOT EXISTS accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role ENUM('Admin', 'VehicleOps', 'Safety Officer', 'Client') NOT NULL,
+    can_create_account BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);";
 
 pub fn create_db(database_path: PathBuf) {
     println!("Creating Database at : {:?}", database_path);
+    // Creating new db file if it doesn't exists
+    if let Some(parent) = database_path.parent() {
+        std::fs::create_dir_all(parent).expect("failed to create directories");
+    }
+    std::fs::File::create(&database_path).expect("failed to create db file");
+    println!("Created DB file at {:?}", database_path);
+
+    // Opening with full acess
+    let flags = OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE;
+    let conn = Connection::open_with_flags(&database_path, flags).expect("DATABASE_ISSUE");
+
+    // IDK WHY THIS IS GOING newline
+    conn.pragma_update(None, "foreign_keys", &"ON")
+        .expect("DATABASE_ISSUE");
+
+    // Createing initial table
+    conn.execute_batch(&INITIAL_SQL_PROMPT)
+        .expect("ERROR_IN_INITIAL_PROMPT");
 }
