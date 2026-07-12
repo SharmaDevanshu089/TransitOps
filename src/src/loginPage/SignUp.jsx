aa0d57c5e497dd3c59786e123692f61cf68bd453
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-12 11:14:37 +0530

    Fixed Login issue

--- src/src/loginPage/SignUp.jsx
+++ src/src/loginPage/SignUp.jsx
@@ -1,11 +1,11 @@
 import { invoke } from "@tauri-apps/api/core";
 
-export function SignIn({ setPage }){
+export function SignIn({ setPage }) {
 
     const handleSubmit = async (e) => {
         //to prevent refresh
         e.preventDefault();
-     
+
         //to get data from form
         const formData = new FormData(e.target);
         const data = Object.fromEntries(formData.entries());
@@ -13,8 +13,8 @@ export function SignIn({ setPage }){
         try {
             // to get the user role from database
             const role = await invoke("authenticate_user", {
-                usernameInput: data.username,
-                passwordInput: data.password,
+                username: data.username,
+                password: data.password,
             });
             // condition to check if role is defined or not 
             if (role) {
@@ -23,7 +23,7 @@ export function SignIn({ setPage }){
             } else {
                 console.log("Login failed: Invalid username or password.");
             }
-        // catch any error
+            // catch any error
         } catch (err) {
             console.error("error:", err);
         }
@@ -32,14 +32,14 @@ export function SignIn({ setPage }){
     return <div>
         <button onClick={() => setPage('none')}>Back</button>
         <form onSubmit={handleSubmit}>
-            <div><label>User Name</label><br/><br/><input type="text" name="username" required /></div>
-            <br/>
-            <div><label>Password</label><br/><br/><input type="password" name="password" required /></div>
-            <br/>
+            <div><label>User Name</label><br /><br /><input type="text" name="username" required /></div>
+            <br />
+            <div><label>Password</label><br /><br /><input type="password" name="password" required /></div>
+            <br />
             <button type="submit">Login</button>
         </form>
-        <br/>
-        <br/>
+        <br />
+        <br />
         <p>Don't have an account? <button type="button" onClick={() => setPage('register')}>Register</button></p>
     </div>
 }
