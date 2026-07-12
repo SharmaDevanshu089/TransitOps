8fcff5fda9d9423c92a331323cfcbae3dc6fdd85
Krishna <Krishnavijaywargiya12@gmail.com>
2026-07-12 10:14:10 +0530

    login page basic setup

---
+++ src/src/loginPage/Register.jsx
@@ -0,0 +1,21 @@
+
+export function Register(){
+    return <div>
+        <div>
+            <label>Full Name</label><br/><br/><input type="text"/>
+        </div>
+        <br/>
+        <div>
+            <label>Password</label><br/><br/><input type="password"/>
+        </div>
+        <br/>
+        <div>
+            <label>Confirm Password</label><br/><br/><input type="password"/>
+        </div>
+        <br/>
+        <button>Register</button>
+        <br/>
+        <br/>
+        <p>Already have an account?<a href="#">Login</a></p>
+    </div>
+}
---
+++ src/src/loginPage/SignUp.jsx
@@ -0,0 +1,12 @@
+
+export function SignUp(){
+    return <div>
+        <div><label>User Name</label><br/><br/><input type="text"/></div>
+        <div><label>Password</label><br/><br/><input type="password"/></div>
+        <br/>
+        <button>Sign Up</button>
+        <br/>
+        <br/>
+        <p>Don't have an account?<a href="#">Register</a></p>
+    </div>
+}
