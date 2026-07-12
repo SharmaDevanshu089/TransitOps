4b10cfc8cfd69ecf4da9db1337006ef3eac85edb
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-12 10:51:41 +0530

    Added Authentication Logic

--- src-tauri/src/authenticate.rs
+++ src-tauri/src/authenticate.rs
@@ -1,12 +1,26 @@
+use rusqlite::params;
 use rusqlite::Connection;
+use rusqlite::OptionalExtension;
 use tauri::Manager;
 
 #[tauri::command]
-pub async fn authenticate_user(app: tauri::AppHandle, username: String, password: String) -> String {
+pub async fn authenticate_user(
+    app: tauri::AppHandle,
+    username: String,
+    password: String,
+) -> Result<Option<String>, String> {
     let db_path = app.path().app_data_dir().unwrap().join("transitops.db");
     let conn = Connection::open(db_path).unwrap();
     println!("Need To authenticate {} at {}", username, password);
 
-    return format!("Hello {} , ", username);
-}
+    let role: Option<String> = conn
+        .query_row(
+            "SELECT role FROM accounts WHERE username = ?1 AND password = ?2",
+            params![username, password],
+            |row| row.get(0),
+        )
+        .optional()
+        .map_err(|e| e.to_string())?;
 
+    Ok(role)
+}
